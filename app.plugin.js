66b9e75d4be48ca7046f47dc2708bef48e57d635
Tushar Khatiwada <khatiwadatushar@outlook.com>
2024-12-26 19:11:32 +0545

    updates to the expo config plugin

--- app.plugin.js
+++ app.plugin.js
@@ -1,1 +1,20 @@
-module.exports = require('./build/plugin/withHealthKit').default;
+const { withPlugins } = require('@expo/config-plugins');
+const withHealthKit = require('./build/plugin/withHealthKit').default;
+
+module.exports = config => {
+  return withPlugins(config, [
+    // Add HealthKit capabilities to Info.plist
+    [
+      withHealthKit,
+      {
+        // Optional: Allow customizing the usage description
+        healthShareUsageDescription:
+          'This app requires access to Apple Health data to export your health and fitness information.',
+        healthUpdateUsageDescription:
+          'This app requires write access to Apple Health to save your health and fitness information.',
+        // Optional: Specify which data types to request by default
+        requiredDataTypes: [],
+      },
+    ],
+  ]);
+};
--- src/plugin/withHealthKit.ts
+++ src/plugin/withHealthKit.ts
@@ -1,43 +1,65 @@
 import {
   ConfigPlugin,
-  withEntitlementsPlist,
   withInfoPlist,
+  withEntitlementsPlist,
   withXcodeProject,
 } from '@expo/config-plugins';
 
-const HEALTHKIT_USAGE_DESCRIPTION =
-  'Allow this app to access your health data for exporting and analysis';
+interface HealthKitPluginProps {
+  healthShareUsageDescription?: string;
+  healthUpdateUsageDescription?: string;
+  requiredDataTypes?: string[];
+}
 
-const withHealthKit: ConfigPlugin = config => {
-  // Add HealthKit entitlements
-  config = withEntitlementsPlist(config, config => {
+const DEFAULT_SHARE_USAGE = 'Allow this app to access your health data';
+const DEFAULT_UPDATE_USAGE = 'Allow this app to save health data';
+
+const withHealthKitPlist: ConfigPlugin<HealthKitPluginProps> = (config, props = {}) => {
+  return withInfoPlist(config, config => {
+    const {
+      healthShareUsageDescription = DEFAULT_SHARE_USAGE,
+      healthUpdateUsageDescription = DEFAULT_UPDATE_USAGE,
+    } = props;
+
+    // Add usage descriptions to Info.plist
+    config.modResults.NSHealthShareUsageDescription = healthShareUsageDescription;
+    config.modResults.NSHealthUpdateUsageDescription = healthUpdateUsageDescription;
+
+    return config;
+  });
+};
+
+const withHealthKitEntitlements: ConfigPlugin = config => {
+  return withEntitlementsPlist(config, config => {
+    // Add HealthKit entitlement
     config.modResults['com.apple.developer.healthkit'] = true;
-    config.modResults['com.apple.developer.healthkit.access'] = ['health-records'];
+    config.modResults['com.apple.developer.healthkit.access'] = [];
+
     return config;
   });
+};
 
-  // Add required Info.plist entries
-  config = withInfoPlist(config, config => {
-    config.modResults.NSHealthShareUsageDescription = HEALTHKIT_USAGE_DESCRIPTION;
-    config.modResults.NSHealthUpdateUsageDescription = HEALTHKIT_USAGE_DESCRIPTION;
-    return config;
-  });
-
-  // Add HealthKit capability to Xcode project
-  config = withXcodeProject(config, async config => {
+const withHealthKitXcodeProject: ConfigPlugin = config => {
+  return withXcodeProject(config, async config => {
     const xcodeProject = config.modResults;
-    const targetUUID = xcodeProject.getFirstTarget().uuid;
 
-    xcodeProject.addCapability(targetUUID, 'HealthKit', {
-      purposeStrings: {
-        NSHealthShareUsageDescription: HEALTHKIT_USAGE_DESCRIPTION,
-        NSHealthUpdateUsageDescription: HEALTHKIT_USAGE_DESCRIPTION,
-      },
-    });
+    // Add HealthKit capability
+    const capabilities =
+      xcodeProject.pbxProjectSection()[xcodeProject.getFirstProject().uuid].attributes
+        .TargetAttributes[xcodeProject.getFirstTarget().uuid].SystemCapabilities;
+
+    capabilities['com.apple.HealthKit'] = {
+      enabled: 1,
+    };
 
     return config;
   });
+};
 
+const withHealthKit: ConfigPlugin<HealthKitPluginProps> = (config, props = {}) => {
+  config = withHealthKitPlist(config, props);
+  config = withHealthKitEntitlements(config);
+  config = withHealthKitXcodeProject(config);
   return config;
 };
 
