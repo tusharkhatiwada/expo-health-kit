587e993e049e982ec9317323581ca2a25787f526
Tushar Khatiwada <khatiwadatushar@outlook.com>
2024-12-26 21:49:53 +0545

    .

--- app.plugin.js
+++ app.plugin.js
@@ -1,2 +1,25 @@
-// This file is used during the build process and not bundled with the app
-module.exports = require('./plugin');
+const { withPlugins } = require('@expo/config-plugins');
+
+// This is the main plugin function that Expo will call
+module.exports = (config, props = {}) => {
+  // Ensure we only run this plugin for iOS
+  if (!config.ios) {
+    return config;
+  }
+
+  return withPlugins(config, [
+    // Add HealthKit capabilities to Info.plist
+    [
+      require('./plugin/withHealthKit'),
+      {
+        healthShareUsageDescription:
+          props.healthShareUsageDescription ||
+          'This app requires access to Apple Health data to export your health and fitness information.',
+        healthUpdateUsageDescription:
+          props.healthUpdateUsageDescription ||
+          'This app requires write access to Apple Health to save your health and fitness information.',
+        requiredDataTypes: props.requiredDataTypes || [],
+      },
+    ],
+  ]);
+};
--- plugin/withHealthKit.js
+++ plugin/withHealthKit.js
@@ -1,16 +1,12 @@
 const { withInfoPlist, withEntitlementsPlist, withXcodeProject } = require('@expo/config-plugins');
 
-const DEFAULT_SHARE_USAGE = 'Allow this app to access your health data';
-const DEFAULT_UPDATE_USAGE = 'Allow this app to save health data';
-
 const withHealthKitPlist = (config, props = {}) => {
   return withInfoPlist(config, config => {
     const {
-      healthShareUsageDescription = DEFAULT_SHARE_USAGE,
-      healthUpdateUsageDescription = DEFAULT_UPDATE_USAGE,
+      healthShareUsageDescription = 'Allow this app to access your health data',
+      healthUpdateUsageDescription = 'Allow this app to save health data',
     } = props;
 
-    // Add usage descriptions to Info.plist
     config.modResults.NSHealthShareUsageDescription = healthShareUsageDescription;
     config.modResults.NSHealthUpdateUsageDescription = healthUpdateUsageDescription;
 
@@ -20,10 +16,8 @@ const withHealthKitPlist = (config, props = {}) => {
 
 const withHealthKitEntitlements = config => {
   return withEntitlementsPlist(config, config => {
-    // Add HealthKit entitlement
     config.modResults['com.apple.developer.healthkit'] = true;
     config.modResults['com.apple.developer.healthkit.access'] = [];
-
     return config;
   });
 };
@@ -31,37 +25,29 @@ const withHealthKitEntitlements = config => {
 const withHealthKitXcodeProject = config => {
   return withXcodeProject(config, async config => {
     const xcodeProject = config.modResults;
-    const targetUuid = xcodeProject.getFirstTarget().uuid;
 
     // Add HealthKit framework
     xcodeProject.addFramework('HealthKit.framework', { weak: true });
 
     // Add HealthKit capability
-    if (xcodeProject.pbxProjectSection()) {
-      const projectSection = xcodeProject.pbxProjectSection()[xcodeProject.getFirstProject().uuid];
-      if (projectSection) {
-        const attributes = projectSection.attributes || {};
-        const targetAttributes = attributes.TargetAttributes || {};
-        const mainTargetAttributes = targetAttributes[targetUuid] || {};
-        const systemCapabilities = mainTargetAttributes.SystemCapabilities || {};
+    const pbxProject = xcodeProject.pbxProjectSection()[xcodeProject.getFirstProject().uuid];
+    if (pbxProject) {
+      const targetUuid = xcodeProject.getFirstTarget().uuid;
+      const targetAttributes = pbxProject.attributes.TargetAttributes || {};
+      const target = targetAttributes[targetUuid] || {};
 
-        mainTargetAttributes.SystemCapabilities = {
-          ...systemCapabilities,
-          'com.apple.HealthKit': {
-            enabled: 1,
-          },
-        };
+      target.SystemCapabilities = target.SystemCapabilities || {};
+      target.SystemCapabilities['com.apple.HealthKit'] = { enabled: 1 };
 
-        targetAttributes[targetUuid] = mainTargetAttributes;
-        attributes.TargetAttributes = targetAttributes;
-        projectSection.attributes = attributes;
-      }
+      targetAttributes[targetUuid] = target;
+      pbxProject.attributes.TargetAttributes = targetAttributes;
     }
 
     return config;
   });
 };
 
+// Main plugin function
 module.exports = (config, props = {}) => {
   if (!config.ios) {
     return config;
@@ -70,5 +56,6 @@ module.exports = (config, props = {}) => {
   config = withHealthKitPlist(config, props);
   config = withHealthKitEntitlements(config);
   config = withHealthKitXcodeProject(config);
+
   return config;
 };
